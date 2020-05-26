098f5e6da1ad65fbb27f0bdd24fef9864d6f5cea
Marudhupandian Ramaiah <31066562+marudhupandian-ramaiah@users.noreply.github.com>
2020-05-27 00:47:46 +0530

    Update CompletableFutureTest.java

--- src/com/learn/core/CompletableFutureTest.java
+++ src/com/learn/core/CompletableFutureTest.java
@@ -12,8 +12,7 @@ public class CompletableFutureTest {
 
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
 
-		CompletableFuture<String> cf = CompletableFuture.supplyAsync(() -> "hello")
-														.thenApply(str -> str.toUpperCase());
+		CompletableFuture<String> cf = CompletableFuture.supplyAsync(() -> "hello").thenApply(str -> str.toUpperCase());
 
 		System.out.println("Result : " + cf.get());
 
